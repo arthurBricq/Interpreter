4d8756ca9d0254ea5d4243d41bc156969d707132
Arthur Bricq <arthur.bricq@bluebotics.com>
2024-03-11 10:21:25 +0100

    Formatting

--- src/module.rs
+++ src/module.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+
 use crate::ast::declaration::Declaration;
 use crate::ast::expression::Value;
 use crate::error::EvalError;
@@ -37,9 +38,11 @@ impl Module {
         }
     }
 }
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
+
     use crate::ast::expression::Value::{BoolValue, IntValue};
     use crate::parser::Parser;
     use crate::token::tokenize;
@@ -73,17 +76,18 @@ mod tests {
         let result = cat.eval(&mut HashMap::new(), Some(&module));
         assert_eq!(result, Ok(IntValue(20)));
     }
-        #[test]
+
+    #[test]
     fn test_returns_true_or_false() {
         let text = std::fs::read_to_string("TestData/if_else_loops.txt").unwrap();
         let tokens = tokenize(&text).unwrap();
         let mut parser = Parser::new(&tokens);
         let module = parser.parse_module();
-            
+
         let returns_true = module.get_function(&"returns_true".to_string()).unwrap();
         let result = returns_true.eval(&mut HashMap::new(), Some(&module));
         assert_eq!(result, Ok(BoolValue(true)));
-        
+
         let returns_false = module.get_function(&"returns_false".to_string()).unwrap();
         let result = returns_false.eval(&mut HashMap::new(), Some(&module));
         assert_eq!(result, Ok(BoolValue(false)));
